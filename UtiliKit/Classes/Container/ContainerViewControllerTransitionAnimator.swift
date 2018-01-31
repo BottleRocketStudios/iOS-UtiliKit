bbf922a4fc993165da4d2b3f25926f3a2dfae911
Will McGinty <will.mcginty@bottlerocketstudios.com>
2018-01-31 09:48:54 -0600

    Make the default animator take no duration

--- UtiliKit/Classes/Container/ContainerViewControllerTransitionAnimator.swift
+++ UtiliKit/Classes/Container/ContainerViewControllerTransitionAnimator.swift
@@ -10,12 +10,12 @@ import UIKit
 class ContainerViewControllerTransitionAnimator: NSObject, UIViewControllerAnimatedTransitioning {
     
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-        return 0.3
+        return 0.0
     }
     
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
         guard let source = transitionContext.view(forKey: .from), let destination = transitionContext.view(forKey: .to) else { return }
-        UIView.transition(from: source, to: destination, duration: transitionDuration(using: transitionContext), options: .transitionCrossDissolve) { finished in
+        UIView.transition(from: source, to: destination, duration: transitionDuration(using: transitionContext), options: []) { finished in
             transitionContext.completeTransition(finished)
         }
     }
