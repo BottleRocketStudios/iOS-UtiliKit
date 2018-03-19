bdcf9e18e32448dbd554dd9a658ba25eecff5311
Wilson Turner <wilson.turner@bottlerocketstudios.com>
2018-03-19 11:35:37 -0500

    Updates Tests Target

---
+++ Example/UtiliKit-iOS/UtiliKit_iOS.h
@@ -0,0 +1,19 @@
+//
+//  UtiliKit_iOS.h
+//  UtiliKit-iOS
+//
+//  Created by Wilson Turner on 3/19/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UtiliKit_iOS.
+FOUNDATION_EXPORT double UtiliKit_iOSVersionNumber;
+
+//! Project version string for UtiliKit_iOS.
+FOUNDATION_EXPORT const unsigned char UtiliKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UtiliKit_iOS/PublicHeader.h>
+
+
