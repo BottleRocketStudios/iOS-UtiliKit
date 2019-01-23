24ee1fded50420ea9e00083e7722ad2eaf007bf9
Will McGinty <will.mcginty@bottlerocketstudios.com>
2019-01-23 16:43:23 -0600

    More documenation, change a few defaults

--- Sources/UtiliKit/Container/Transitioning/Interaction/ContainerPercentDrivenInteractiveTransition.swift
+++ Sources/UtiliKit/Container/Transitioning/Interaction/ContainerPercentDrivenInteractiveTransition.swift
@@ -18,18 +18,38 @@ open class ContainerPercentDrivenInteractiveTransition: NSObject, UIViewControll
     }
 
     // MARK: Properties
+    
+    /// completionSpeed defaults to 1.0 which corresponds to a completion duration of
+    /// (1 - percentComplete)*duration.  It must be greater than 0.0. The actual
+    /// completion is inversely proportional to the completionSpeed.  This can be set
+    /// before cancelInteractiveTransition or finishInteractiveTransition is called
+    /// in order to speed up or slow down the non interactive part of the
+    /// transition.
     open var completionSpeed: CGFloat = 1
-    open var completionCurve: UIView.AnimationCurve = .linear
+    
+    /// When the interactive part of the transition has completed, this property can
+    /// be set to indicate a different animation curve. It defaults to UIViewAnimationCurveEaseInOut.
+    /// Note that during the interactive portion of the animation the timing curve is linear.
+    open var completionCurve: UIView.AnimationCurve = .easeInOut
+    
+    /// This is the non-interactive duration that was returned when the
+    /// animators transitionDuration: method was called when the transition started.
     open var duration: CGFloat {
         return CGFloat(transitionAnimator?.transitionDuration(using: transitionContext) ?? 0)
     }
+    
+    /// The last percentComplete value specified by updateInteractiveTransition:
     open var percentComplete: CGFloat = 0 {
         didSet { transitionContext?.updateInteractiveTransition(percentComplete) }
     }
     
+    /// The animator object used to drive the transition. It will be linearly interpolated using the gesture powering the interactive portion of the transition, and will continue to completion once interaction has ended.
     open weak var transitionAnimator: UIViewControllerAnimatedTransitioning?
+    
+    /// The context used to configure the transition.
     open weak var transitionContext: UIViewControllerContextTransitioning?
     
+    /// The current state of the transition - inactive, interacting or completing (animating).
     private(set) public var state: State = .inactive
     
     // MARK: Initializers
@@ -39,6 +59,10 @@ open class ContainerPercentDrivenInteractiveTransition: NSObject, UIViewControll
     }
 
     // MARK: UIViewControllerInteractiveTransitioning
+    
+    /// Starts an interactive transition. Sets up the animation for the transition, allowing the user to step through it by supplying different 'percentComplete' values to update(:)
+    ///
+    /// - Parameter context: The context used to configure the transition
     open func startInteractiveTransition(_ context: UIViewControllerContextTransitioning) {
         guard transitionAnimator != nil, state == .inactive else { return }
         
@@ -46,6 +70,9 @@ open class ContainerPercentDrivenInteractiveTransition: NSObject, UIViewControll
         transitionContext = context
     }
     
+    /// Update the state of the transition to a new percentage. Allowing the object to drive the underlying transition animator to a new visual state.
+    ///
+    /// - Parameter complete: The completion percentage of the transition. A floating point value from 0.0-1.0.
     open func update(percentComplete complete: CGFloat) {
         guard state == .interacting else { return }
         
@@ -53,6 +80,7 @@ open class ContainerPercentDrivenInteractiveTransition: NSObject, UIViewControll
         percentComplete = normalizedPercent
     }
     
+    /// Cancel the interactive transition, instructing the transition to return to it's initial state.
     open func cancel() {
         guard state == .interacting else { return }
         
@@ -60,6 +88,7 @@ open class ContainerPercentDrivenInteractiveTransition: NSObject, UIViewControll
         transitionContext?.cancelInteractiveTransition()
     }
     
+    /// Finish the transition through animation, instructing the transition to complete it's movement to the destination view controller.
     open func finish() {
         guard state == .interacting else { return }
         
