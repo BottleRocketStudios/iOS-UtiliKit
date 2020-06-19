ae1236a5d4f601376d50762b8d57fd7f62b1b6cf
russellm <russell@mirabellifamily.org>
2020-06-19 13:03:18 -0500

    Basic implementation complete with simple unit tests.

--- Tests/LanguageFilterTests.swift
+++ Tests/LanguageFilterTests.swift
@@ -20,4 +20,42 @@ class LanguageFilterTests: XCTestCase {
         XCTAssert(!containsOffensiveLanguage, "Should not trigger the offensive language filter")
     }
 
+    func test_yesOffensiveLanguageDoesReturnTrue() {
+        // arrange
+        let offensive = "ass"
+        // act
+        let containsOffensiveLanguage = offensive.containsOffensiveLanguage
+        // assert
+        XCTAssert(containsOffensiveLanguage, "Should not trigger the offensive language filter")
+    }
+
+    func test_replacesWithCorrectLength() {
+        //arrange
+        let offensive = "bite my shiny metal ass"
+        // act
+        let replaced = offensive.removingOffensiveLanguage
+        let asterisks = replaced.filter { $0 == "*" }
+        // assert
+        XCTAssert(asterisks.count == 3, "Incorrect replacement length in \(replaced)")
+    }
+
+    func test_doesNotReplaceInoffensive() {
+        // arrange
+        let unoffensive = "my hovercraft is full of eels"
+        // act
+        let replaced = unoffensive.removingOffensiveLanguage
+        // assert
+        XCTAssert(!replaced.contains("*"), "Incorrect replacement in \(replaced)")
+    }
+
+    func test_replacesWithAlternateCharacter() {
+        //arrange
+        let offensive = "bite my shiny metal ass"
+        // act
+        let replaced = offensive.replacingOffensiveWords(with: "•")
+        let asterisks = replaced.filter { $0 == "•" }
+        // assert
+        XCTAssert(asterisks.count == 3, "Incorrect replacement length in \(replaced)")
+    }
+
 }
