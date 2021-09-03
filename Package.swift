d29a6ec21248ef9662eb89b7ef326962667951df
Will McGinty <will.mcginty@bottlerocketstudios.com>
2021-09-03 16:24:51 -0500

    Remove SnapshotTesting dependency

--- Package.swift
+++ Package.swift
@@ -17,44 +17,28 @@ let package = Package(
             name: "UtiliKit-Core",
             targets: ["Instantiate", "General"]),
         ],
-    dependencies: [
-        .package(
-            url: "https://github.com/pointfreeco/swift-snapshot-testing",
-            from: "1.9.0")
-        ],
     targets: [
         .target(
             name: "General",
-            dependencies: [],
             path: "Sources/UtiliKit/General"),
         .target(
             name: "Instantiate",
-            dependencies: [],
             path: "Sources/UtiliKit/Instantiation"),
         .target(
             name: "TimelessDate",
-            dependencies: [],
             path: "Sources/UtiliKit/TimelessDate"),
         .target(
             name: "Version",
-            dependencies: [],
             path: "Sources/UtiliKit/Version"),
         .target(
             name: "Container",
-            dependencies: [],
             path: "Sources/UtiliKit/Container"),
         .target(
             name: "ActiveLabel",
-            dependencies: [],
             path: "Sources/UtiliKit/ActiveLabel"),
         .target(
             name: "Obfuscation",
-            dependencies: [],
-            path: "Sources/UtiliKit/Obfuscation"),
-        .testTarget(
-            name: "UtiliKitTests",
-            dependencies: ["Instantiate", "TimelessDate", "General", "Version", "Container", "ActiveLabel", "Obfuscation", "SnapshotTesting"],
-            path: "Tests")
+            path: "Sources/UtiliKit/Obfuscation")
     ]
 )
 
