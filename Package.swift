d66a746568fa93dcbefdd1d96c5d591fbf800485
Will McGinty <will.mcginty@bottlerocketstudios.com>
2021-10-04 13:38:45 -0500

    Tweak target/library names

--- Package.swift
+++ Package.swift
@@ -9,35 +9,50 @@ let package = Package(
     products: [
         .library(
             name: "UtiliKit",
-            targets: ["GeneralUtilities", "InstantiationUtilities", "TimelessDate", "VersionUtilities", "ContainerViewController", "ActiveLabel", "ObfuscationUtilities"]),
+            targets: ["GeneralUtilities", "Instantiation", "TimelessDate", "Versioning", "Container", "ActiveLabel", "Obfuscation"]),
         .library(
-            name: "UtiliKitContainer",
-            targets: ["ContainerViewController"]),
+            name: "GeneralUtilities",
+            targets: ["GeneralUtilities"]),
         .library(
-            name: "UtiliKitCore",
-            targets: ["InstantiationUtilities", "GeneralUtilities"]),
+            name: "Instantiation",
+            targets: ["Instantiation"]),
+        .library(
+            name: "TimelessDate",
+            targets: ["TimelessDate"]),
+        .library(
+            name: "Versioning",
+            targets: ["Versioning"]),
+        .library(
+            name: "Container",
+            targets: ["Container"]),
+        .library(
+            name: "ActiveLabel",
+            targets: ["ActiveLabel"]),
+        .library(
+            name: "Obfuscation",
+            targets: ["Obfuscation"]),
         ],
     targets: [
         .target(
             name: "GeneralUtilities",
             path: "Sources/UtiliKit/General"),
         .target(
-            name: "InstantiationUtilities",
+            name: "Instantiation",
             path: "Sources/UtiliKit/Instantiation"),
         .target(
             name: "TimelessDate",
             path: "Sources/UtiliKit/TimelessDate"),
         .target(
-            name: "VersionUtilities",
+            name: "Versioning",
             path: "Sources/UtiliKit/Version"),
         .target(
-            name: "ContainerViewController",
+            name: "Container",
             path: "Sources/UtiliKit/Container"),
         .target(
             name: "ActiveLabel",
             path: "Sources/UtiliKit/ActiveLabel"),
         .target(
-            name: "ObfuscationUtilities",
+            name: "Obfuscation",
             path: "Sources/UtiliKit/Obfuscation")
     ]
 )
